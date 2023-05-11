2915c375836fde679de34a085d9f3ee3310e1dc2
73ddy <golubinov.misha@gmail.com>
2023-05-11 23:35:47 +0400

    Update

--- MainPage.php
+++ MainPage.php
@@ -144,8 +144,27 @@
         <script src = "smooth-scrollbar.js"></script>
     </body>
     </html>
-<?php elseif ($_SESSION['auth'] == true): ?>
-	<p>пожалуйста, авторизуйтесь</p>
+<?php elseif ($_SESSION['auth'] == false): ?>
+	<!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="Main.css"/>
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.3.0/css/all.min.css" integrity="sha512-SzlrxWUlpfuzQ+pcUCosxcglQRNAq/DZjVsC0lE40xsADsfeQoEypE+enwcOiGjk/bSuGGKHEyjSoQ1zVisanQ==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+        <title>Task Manager</title>
+    </head>
+    <body>
+        <div class="main-access-denied">
+            <p class="access-denied">
+                Access denied!
+            </p>
+        </div>
+        <!-- <div class="overlay"></div> -->
+        <script src = "script.js"></script>
+    </body>
+    </html>
 <?php endif; ?>
 
 
